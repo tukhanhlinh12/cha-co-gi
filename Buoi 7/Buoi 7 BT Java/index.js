6a9076fc5ecb776f197d33ba181b2b9f30152e2b
Từ Khánh Linh <132938285+tukhanhlinh12@users.noreply.github.com>
2023-05-27 09:07:20 +0700

    Add files via upload

---
+++ Buoi 7/Buoi 7 BT Java/index.js
@@ -0,0 +1,38 @@
+//----Bài 1---
+
+/*const name= prompt("Nhập tên của bạn:");
+console.log("Tên của bạn là: " + name)*/
+
+//---Bài 2---
+
+/*var name = prompt("Nhập tên của bạn:");
+var uppercaseName = name.toUpperCase();
+console.log("Tên của bạn là: " + uppercaseName);*/
+
+//--Bài 3--
+ 
+/*const firstName= prompt("Nhập họ của bạn: ");
+const lastName= prompt("Nhập tên của bạn: ");
+alert(`Tên của bạn là: ${firstName} ${lastName}`);*/
+
+//--Bài 4--
+
+/*const firstNumber = prompt("Nhập số đầu:")
+const secondNumer = prompt("Nhập số thứ hai:")
+const tong = Number(firstNumber) + Number(secondNumer)
+alert(`Tổng hai số là: ${tong}`);*/
+
+//---Bài 5---
+
+/*var number = prompt("Nhập số cần tính:");
+// Kiểm tra xem người dùng đã nhập số hay chưa
+if (!isNaN(number)) {
+  var remainder = number % 2;
+  alert("Phần dư khi chia " + number + " cho 2 là: " + remainder);
+} else {
+  alert("Vui lòng nhập một số hợp lệ!");
+}*/
+
+
+
+
